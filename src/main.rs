a2b6814c411b390fff7abc065860db139863a8e5
kangsl <kangsl@weipaitang.com>
2020-11-04 14:30:36 +0800

    traits and display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fmt::{
+    Display,
+    Error,
+    Formatter,
+};
+
+
+struct Person {
+    name: String,
+    age: u8,
+}
+
+impl Display for Person {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "{}, {}", self.name, self.age)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let leo = Person {
+        name: "leo".to_string(),
+        age: 25
+    };
+    println!("Hello {}", leo);
 }
