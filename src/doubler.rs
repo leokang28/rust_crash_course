79a653e5086d3ef20f2ecb6d4ea18e3a51c42553
kangsl <kangsl@weipaitang.com>
2020-11-07 16:29:34 +0800

    where

---
+++ src/doubler.rs
@@ -0,0 +1,29 @@
+struct Doubler<I> {
+    iter: I,
+}
+
+impl<I> Iterator for Doubler<I>
+    where
+        I: Iterator,
+        I::Item: std::ops::Add<Output=I::Item> + Copy {
+    type Item = I::Item;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next() {
+            None => None,
+            Some(v) => Some(v + v),
+        }
+    }
+}
+
+fn run() {
+    let iter = Doubler {
+        iter: 1..11,
+    };
+    for i in iter {
+        println!("{}", i);
+    }
+}
+
+
+pub fn main() {
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,13 @@
-use std::fmt::{
-    Display,
-    Error,
-    Formatter,
-};
-
 mod iterator;
 
 mod fibs;
-
-struct Person {
-    name: String,
-    age: u8,
-}
-
-impl Display for Person {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        write!(f, "{}, {}", self.name, self.age)
-    }
-}
+mod doubler;
 
 fn main() {
     // iterator::main();
     // fibs::main();
-    let a: u8 = 254;
-    match a.checked_add(2) {
-        None => println!("overflow, max of u8 is {}", u8::MAX),
-        Some(v) => println!("{}", v),
-    };
+    doubler::main();
+
 }
 
 
